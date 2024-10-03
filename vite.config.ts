de9d3d8c0968b72b1a507a2bab6dc911117b15e9
Pouya <pouya@kenshi.io>
2024-10-03 18:13:57 +0200

    Switch to Geist font

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,23 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import unfonts from 'unplugin-fonts/vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		unfonts({
+			custom: {
+				families: [
+					{
+						name: 'Geist',
+						src: './node_modules/geist/fonts/geist-sans/*.woff2'
+					},
+					{
+						name: 'Geist Mono',
+						src: './node_modules/geist/fonts/geist-mono/*.woff2'
+					}
+				]
+			}
+		})
+	]
 });
