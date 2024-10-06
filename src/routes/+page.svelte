3ad606f1320baef9c053570e9607818c678bc0fc
Pouya <pouya@kenshi.io>
2024-10-06 20:31:45 +0200

    Fix for smaller screens

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,7 +12,11 @@
 	import Input from '$lib/forms/Input.svelte';
 </script>
 
-<Section class="w-full max-w-[1200px] mx-auto pt-12 !gap-16 px-4 md:px-0">
+<svelte:head>
+	<title>Timeleap | Distributed App Engine</title>
+</svelte:head>
+
+<Section class="w-full max-w-[1200px] mx-auto pt-12 !gap-16 px-4 xl:px-0">
 	<Navbar class="bg-zinc-900 text-white">
 		<Button class="bg-cyan-900">Home</Button>
 		<Button class="hover:bg-zinc-800">
@@ -39,23 +43,23 @@
 		<Image src="/images/robot.jpg" alt="Distributed Computation" height="min(640px, 100vw)" />
 	</Grid>
 	<h2 class="white-text text-white font-serif text-5xl">Discover.</h2>
-	<Grid extraLargeScreenColumns={3}>
+	<Grid extraLargeScreenColumns={3} largeScreenColumns={2}>
 		<Card class="!p-0 flex bg-violet-100">
-			<div class="p-8 flex-1 content-center">
+			<div class="px-8 flex-1 content-center">
 				<h3 class="font-serif text-2xl">Unchained</h3>
-				<div class="text-gray-800">Distributed Computation</div>
+				<div class="text-gray-800">Distributed App Engine</div>
 			</div>
 			<Image src="/images/pattern1.svg" alt="Unchained" width="128px" height="128px"></Image>
 		</Card>
 		<Card class="!p-0 flex bg-red-100">
-			<div class="p-8 flex-1 content-center">
+			<div class="px-8 flex-1 content-center">
 				<h3 class="font-serif text-2xl">GhostFS</h3>
 				<div class="text-gray-800">Remote File Storage</div>
 			</div>
 			<Image src="/images/pattern2.png" alt="Unchained" width="128px" height="128px"></Image>
 		</Card>
 		<Card class="!p-0 flex bg-green-100">
-			<div class="p-8 flex-1 content-center">
+			<div class="px-8 flex-1 content-center">
 				<h3 class="font-serif text-2xl">Cascade</h3>
 				<div class="text-gray-800">Blockchain as a Service</div>
 			</div>
@@ -64,7 +68,7 @@
 	</Grid>
 </Section>
 
-<Section class="w-full max-w-[1200px] mx-auto pt-24 !gap-16 pb-24 px-4 md:px-0">
+<Section class="w-full max-w-[1200px] mx-auto pt-24 !gap-16 pb-24 px-4 xl:px-0">
 	<Grid extraLargeScreenColumns={5} largeScreenColumns={5} class="mt-8">
 		<Image
 			src="/images/rolle.jpg"
@@ -127,7 +131,7 @@
 	</Card>
 </Section>
 
-<Section class="max-w-[1200px] mx-auto">
+<Section class="max-w-[1200px] mx-auto px-4 xl:px-0">
 	<Grid
 		extraLargeScreenColumns={4}
 		largeScreenColumns={4}
