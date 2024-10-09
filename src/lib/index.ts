78945c3b9f59966bf12df5210ecce8eb3ce1d61f
Andrey Luiz <andrey@timeleap.swiss>
2024-10-09 14:37:48 +0200

    First release

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,8 @@
 // Reexport your entry components here
+export { default as Input } from './forms/Input.svelte';
+export { default as Grid } from './layout/Grid.svelte';
+export { default as Section } from './layout/Section.svelte';
+export { default as Button } from './ui/Button.svelte';
+export { default as Card } from './ui/Card.svelte';
+export { default as Image } from './ui/Image.svelte';
+export { default as Navbar } from './ui/Navbar.svelte';
